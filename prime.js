c5eb29860a05617f7a654770cc953f467dbbd33a
Creed Camp <creedomed@gmail.com>
2020-09-09 08:56:44 -0700

    final save

--- prime.js
+++ prime.js
@@ -1,6 +1,13 @@
-function setDate() {
+const colorChange = setInterval(() => {
   let TI = new Date()
-  let seconds = TI.getSeconds()
-  console.log(seconds)
-}
-setInterval(setDate, 1000)
+  let seconds = TI.getSeconds
+  let colors = "#count".color[i]
+  document.querySelector("#count").innerHTML = seconds + colors
+}, 1000)
+
+const change = setTimeout(() => {
+  if (setInterval)
+  let colors = "#count".color
+  document.querySelector("#count").innerHTML = seconds + colors++
+  console.log("check")
+}, 2000)
