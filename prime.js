dd5b4438f708da08ecf1ef58e5963253b9707050
Creed Camp <creedomed@gmail.com>
2020-09-18 11:50:31 -0700

    saving the stuff

--- prime.js
+++ prime.js
@@ -1,13 +1,23 @@
-const colorChange = setInterval(() => {
+const colorTime = setInterval(() => {
   let TI = new Date()
-  let seconds = TI.getSeconds
-  let colors = "#count".color[i]
-  document.querySelector("#count").innerHTML = seconds + colors
+  let seconds = TI.getSeconds()
+  let minutes = TI.getMinutes()
+  let hours = TI.getHours()
+  if (hours > 12) {
+    hours = hours() - 12
+  } else {
+    hours = hours
+  }
+  ;(hours) => (hours = 0 ? "0" + hours : hours)
+  ;(minutes) => (minutes = 0 ? minutes : "0" + minutes)
+  ;(seconds) => (seconds = 0 ? seconds : "0" + seconds)
+  let showTime = hours + ":" + minutes + ":" + seconds
+  // let colors = "#count".color[i]
+  document.querySelector("#count").innerHTML = showTime
 }, 1000)
 
 const change = setTimeout(() => {
-  if (setInterval)
-  let colors = "#count".color
-  document.querySelector("#count").innerHTML = seconds + colors++
-  console.log("check")
+  if (colorTime) {
+  }
+  // let colors = "#count".color
 }, 2000)
